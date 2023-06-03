50b85d6f5e8ea4a0f1c90a5c2f7f93b2ddf6905d
Shivam Baronia <shivambaronia007@gmail.com>
2023-06-03 19:54:43 +0530

    Display temp/sky conditions,handled error

--- backend.py
+++ backend.py
@@ -1,18 +1,14 @@
 import requests 
 API_KEY = '65c33b6ed2fb82d5db24b143ad971410'
 
-def get_data(place,days,option):
+def get_data(place,days):
     urls = f'http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}'
     response = requests.get(url=urls)
     data = response.json() 
     filtered_data = data['list']
     nr_values = 8*days
-    filtered_data = filtered_data[:nr_values]
-    if option == 'Temperature':
-        filtered_data = [dict['main']['temp'] for dict in filtered_data]
-    if option == 'Sky':
-        filtered_data = [data['weather'][0]['main'] for dict in filtered_data]
+    filtered_data = filtered_data[:nr_values]    
     return filtered_data
 
 if __name__ == '__main__':
-    print(get_data(place='Tokyo',days=2,option='Temperature'))   
+    print(get_data(place='Tokyo',days=2))   
--- main.py
+++ main.py
@@ -21,7 +21,26 @@ option = st.selectbox(label='Select data to view:',
 # Displaying the subheader
 st.subheader(f'{option} for next {days} days in {place}')
 
-# Calling the function from backend
-d,t = get_data()
-figure = px.line(x=d,y=t,label={'x':'Date','y':'Temperature (C)'})
-st.plotly_chart(figure)
+if place:
+    try:
+        # Get temperature/sky data
+        filtered_data = get_data(place=place,days=days)
+
+        if option == 'Temperature':
+            temperatures = [dict['main']['temp'] / 10 for dict in filtered_data]
+            dates = [dict['dt_txt'] for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates,y=temperatures,labels={'x':'Date','y':'Temperature (C)'})
+            st.plotly_chart(figure)
+
+        if option == 'Sky':
+            images_path = {'Clear':'images/clear.png',
+                    'Clouds':'images/cloud.png',
+                    'Rain':'images/rain.png',
+                    'Snow':'images/snow.png'}
+            sky_conditions = filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
+            images = [images_path[condition] for condition in sky_conditions]
+            # print(sky_conditions)
+            st.image(images,width=100)
+    except KeyError:
+        st.write('That place does not exist.')
