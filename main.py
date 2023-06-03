4da8373bf2e023171d4e236eb9613e35b4d56b8a
Shivam Baronia <shivambaronia007@gmail.com>
2023-06-03 18:40:33 +0530

    Added backend for temp and main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st 
+import plotly.express as px
+from backend import get_data
 
 st.set_page_config(layout='wide')
 heading1 = 'Wether Forecast for next few days'
@@ -14,7 +16,12 @@ days = st.slider(min_value=1,max_value=5,
 
 # Select data to view
 option = st.selectbox(label='Select data to view:',
-                      options=['Temperature','Rain'])
+                      options=['Temperature','Sky'])
 
 # Displaying the subheader
 st.subheader(f'{option} for next {days} days in {place}')
+
+# Calling the function from backend
+d,t = get_data()
+figure = px.line(x=d,y=t,label={'x':'Date','y':'Temperature (C)'})
+st.plotly_chart(figure)
